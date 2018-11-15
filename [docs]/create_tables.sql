17fb00017169187ceef3526e814f10c53131d79a
Vitor Dullens <vitordullens2.0@gmail.com>
2018-11-15 19:16:08 -0200

    ve se o serial funciona

--- [docs]/create_tables.sql
+++ [docs]/create_tables.sql
@@ -1,55 +1,55 @@
 -- company table
 CREATE TABLE company
     (
-        company_id SERIAL PRIMARY KEY,
+        company_id PRIMARY KEY SERIAL,
         company_name VARCHAR(50) NOT NULL
     );
 -- fake news type table
 CREATE TABLE fake_news_type
     (
-        fake_news_type_id SERIAL PRIMARY KEY,
+        fake_news_type_id PRIMARY KEY SERIAL,
         fake_news_type_name VARCHAR(50) NOT NULL
     );
 -- government power table
 CREATE TABLE government_power
     (
-        government_power_id SERIAL PRIMARY KEY,
+        government_power_id PRIMARY KEY SERIAL,
         government_power_name VARCHAR(50) NOT NULL
     );
 -- usuario table 
 CREATE TABLE usuario 
     (
-        usuario_id SERIAL PRIMARY KEY,
+        usuario_id PRIMARY KEY SERIAL,
         usuario_name VARCHAR(50) NOT NULL
     );
 -- tabela partidos
 CREATE TABLE parties 
     (
-        parties_id SERIAL PRIMARY KEY,
+        parties_id PRIMARY KEY SERIAL,
         parties_name VARCHAR(50) NOT NULL
     );
 -- metodo de propagação table
 CREATE TABLE propagation_method
     (
-        propagation_method_id SERIAL PRIMARY KEY,
+        propagation_method_id PRIMARY KEY SERIAL,
         propagation_method_name VARCHAR(50) NOT NULL
     );
 -- penalidade table
 CREATE TABLE penalty_type
     (
-        penalty_type_id SERIAL PRIMARY KEY,
+        penalty_type_id PRIMARY KEY SERIAL,
         penalty_type_name VARCHAR(50) NOT NULL
     );
 -- extensao table
 CREATE TABLE extensao
     (
-        extensao_id SERIAL PRIMARY KEY,
+        extensao_id PRIMARY KEY SERIAL,
         extensao_name VARCHAR(50) NOT NULL
     );
 -- fake news table
 CREATE TABLE fake_news
     (
-        fake_news_id SERIAL PRIMARY KEY,
+        fake_news_id PRIMARY KEY SERIAL,
         fake_news_title VARCHAR(200) NOT NULL,
         fake_news_content TEXT NOT NULL,
         fake_news_intention BOOLEAN NOT NULL,
@@ -67,7 +67,7 @@ CREATE TABLE fake_news_usuario
 -- tabela dos arquivos das fake news
 CREATE TABLE arquivo
     (
-        arquivo_id SERIAL PRIMARY KEY,
+        arquivo_id PRIMARY KEY SERIAL,
         arquivo_content BYTEA NOT NULL,
         fake_news_id INTEGER REFERENCES fake_news(fake_news_id),
         extensao_id INTEGER REFERENCES extensao(extensao_id)
