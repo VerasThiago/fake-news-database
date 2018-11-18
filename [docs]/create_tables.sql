d78660bb1cffdc38cd8e8670ff731c23327a1bd8
GabrielBessa <gabrielbessarofl@gmail.com>
2018-11-17 23:56:23 -0200

    Rearranjed 'SERIAL' so it works properly

--- [docs]/create_tables.sql
+++ [docs]/create_tables.sql
@@ -1,55 +1,55 @@
 -- company table
 CREATE TABLE company
     (
-        company_id PRIMARY KEY SERIAL,
+        company_id SERIAL PRIMARY KEY,
         company_name VARCHAR(50) NOT NULL
     );
 -- fake news type table
 CREATE TABLE fake_news_type
     (
-        fake_news_type_id PRIMARY KEY SERIAL,
+        fake_news_type_id SERIAL PRIMARY KEY,
         fake_news_type_name VARCHAR(50) NOT NULL
     );
 -- government power table
 CREATE TABLE government_power
     (
-        government_power_id PRIMARY KEY SERIAL,
+        government_power_id SERIAL PRIMARY KEY,
         government_power_name VARCHAR(50) NOT NULL
     );
 -- usuario table 
 CREATE TABLE usuario 
     (
-        usuario_id PRIMARY KEY SERIAL,
+        usuario_id SERIAL PRIMARY KEY,
         usuario_name VARCHAR(50) NOT NULL
     );
 -- tabela partidos
 CREATE TABLE parties 
     (
-        parties_id PRIMARY KEY SERIAL,
+        parties_id SERIAL PRIMARY KEY,
         parties_name VARCHAR(50) NOT NULL
     );
 -- metodo de propagação table
 CREATE TABLE propagation_method
     (
-        propagation_method_id PRIMARY KEY SERIAL,
+        propagation_method_id SERIAL PRIMARY KEY,
         propagation_method_name VARCHAR(50) NOT NULL
     );
 -- penalidade table
 CREATE TABLE penalty_type
     (
-        penalty_type_id PRIMARY KEY SERIAL,
+        penalty_type_id SERIAL PRIMARY KEY,
         penalty_type_name VARCHAR(50) NOT NULL
     );
 -- extensao table
 CREATE TABLE extensao
     (
-        extensao_id PRIMARY KEY SERIAL,
+        extensao_id SERIAL PRIMARY KEY,
         extensao_name VARCHAR(50) NOT NULL
     );
 -- fake news table
 CREATE TABLE fake_news
     (
-        fake_news_id PRIMARY KEY SERIAL,
+        fake_news_id SERIAL PRIMARY KEY,
         fake_news_title VARCHAR(200) NOT NULL,
         fake_news_content TEXT NOT NULL,
         fake_news_intention BOOLEAN NOT NULL,
