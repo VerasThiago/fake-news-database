c9b0c8c3fb9a357afe90906a65b2d5ba926b849e
GabrielBessa <gabrielbessarofl@gmail.com>
2018-11-29 00:55:13 -0200

    Revert "Merge branch 'master' of https://github.com/VerasThiago/fake-news-database"
    This reverts commit e4150b921847851dd31ed7f0ace2ccba4b620750, reversing
    changes made to 63f67e59d94aa1b591cef704c7f3fc1653308171.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,8 +7,8 @@ var dbConn = function () {
   const client = new Client({
     user: 'postgres',
     host: 'localhost',
-    database: '',
-    password: '',
+    database: 'BD_Master',
+    password: 'sapo464',
     port: 5432,
   });
 
